926ad52a8c8260bc4baf4a88ac5266fc0f688d8e
DivyViradiya07 <divya.viradiya.7@gmail.com>
2026-02-22 09:54:59 +0530

    Update

--- extensions.py
+++ extensions.py
@@ -1,6 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from celery import Celery
 
 # Initialize these here, but connect them to the app in run.py
 db = SQLAlchemy()
 login_manager = LoginManager()
+
+def make_celery(app_name=__name__):
+    return Celery(
+        app_name,
+        backend='redis://localhost:6379/0',
+        broker='redis://localhost:6379/0'
+    )
+
+celery = make_celery()
